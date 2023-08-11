9a1da0c166fd9a9cbd03dc5a175f2dcbe558bd9c
sergiiliuk <sergiiliuk@yahoo.dk>
2023-08-11 20:25:48 +0200

    feat: set root element to 16px

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Renderer2 } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
-  title = 'my-app';
+export class AppComponent implements OnInit {
+  constructor(private renderer: Renderer2) {}
+  title = 'Capibara client';
+  ngOnInit(): void {
+    // Set the rem value for the <html> element
+    this.renderer.setStyle(document.documentElement, 'font-size', '16px'); // You can adjust the value as needed
+  }
 }
