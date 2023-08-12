fbe5c80e9cd8edb9ca504082a3617495f36361cf
sergiiliuk <sergiiliuk@yahoo.dk>
2023-08-12 10:49:31 +0200

    feat: add map-libre-gl

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,10 +1,44 @@
-import { Component } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+  OnDestroy,
+} from '@angular/core';
+import { Map, NavigationControl, Marker } from 'maplibre-gl';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.scss']
+  styleUrls: ['./map.component.scss'],
 })
 export class MapComponent {
+  map: Map | undefined;
 
+  @ViewChild('map')
+  private mapContainer!: ElementRef<HTMLElement>;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  ngAfterViewInit() {
+    const initialState = { lng: 139.753, lat: 35.6844, zoom: 14 };
+
+    this.map = new Map({
+      container: this.mapContainer.nativeElement,
+      style: `https://api.maptiler.com/maps/streets-v2/style.json?key=meIz87U0Ci2zKW6N0Saq`,
+      center: [initialState.lng, initialState.lat],
+      zoom: initialState.zoom,
+    });
+    this.map.addControl(new NavigationControl(), 'top-right');
+    new Marker({ color: '#FF0000' })
+      .setLngLat([139.7525, 35.6846])
+      .addTo(this.map);
+  }
+
+  ngOnDestroy() {
+    this.map?.remove();
+  }
 }
